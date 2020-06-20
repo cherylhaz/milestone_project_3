c2700a2aae17f20ce89eb5ca72e1b5626eade73b
cherylhaz <cheryl.hazenberg@gmail.com>
2020-06-20 11:35:17 +0000

    Added add Recipe form

--- app.py
+++ app.py
@@ -2,14 +2,21 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 
-
 app = Flask(__name__)
+
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('add_recipe.html')
+
 
 @app.route('/')
 def main_page():
     return render_template('index.html')
 
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
+
