ad53a0c0a96808ca0f619dac54c76829a2d7be18
cherylhaz <cheryl.hazenberg@gmail.com>
2020-06-23 14:46:29 +0000

    Outline for main page, add recipe connection

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_pymongo import PyMongo
 
 from os import path
 if path.exists("env.py"):
-    import env 
+    import env
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'recipe_box'
@@ -17,16 +17,22 @@ mongo = PyMongo(app)
 def add_recipe():
     return render_template('add_recipe.html')
 
-
-@app.route('/')
-def main_page():
-    return render_template('index.html',
-    categories = mongo.db.categories.find())
-
-
 @app.route('/search_results')
 def search_results():
     return render_template("search_results.html", recipes=mongo.db.recipes.find())
+
+
+#adds new task to database
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('search_results')) 
+
+
+@app.route('/')
+def main_page():
+    return render_template('index.html', categories=mongo.db.categories.find())
 
 
 if __name__ == '__main__':
