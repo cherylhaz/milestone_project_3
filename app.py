2a210a63cd3220f3157c5c41e350e724079259a0
cherylhaz <cheryl.hazenberg@gmail.com>
2020-07-16 09:19:24 +0000

    ongoing search results

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ mongo = PyMongo(app)
 # Search Results
 @app.route('/search_results', methods=['GET', 'POST'])
 def search_results():
-    return render_template("search_results.html", recipes=mongo.db.recipes.find())
+    selected_category = request.form.get("selected_category")
+    return render_template("search_results.html", recipes=mongo.db.recipes.find({'category': selected_category}))
 
 
 # Add New Recipe to Database
